22939b17ce7221ada58d0dc9c11fce102c8be9af
Flipapa <roamer2010x@gmail.com>
2021-07-06 14:00:04 +0800

    Create views: main & index

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send(`<h1>This is a Password Generator!</h1>`)
+  res.render('index')
 })
 
 app.listen(port, () => {
